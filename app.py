c3e6eac4ca3b1dec331c4ea5489437f6589683ed
Karmen Too <karmen.tanaka@gmail.com>
2022-10-20 15:22:16 -0400

    not yet

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify, redirect, render_template, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql:///outer_space'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql:///pets_db'
 app.config['SQLALCHEMY_ECHO'] = True #so we know what went wrong, prints out actual queries being run
 app.config['SECRET_KEY'] = 'ohsosecret'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
--- models.py
+++ models.py
@@ -12,6 +12,9 @@ def connect_db(app):
 class Pet(db.Model):
     __tablename__ = "pets"
     
+    def __repr__(self):
+        return f"<Pet id={self.id} name={self.name} species={self.species} hunger={self.hunger}>"
+    
     id=db.Column(db.Integer,
                  primary_key=True,
                  autoincrement=True
